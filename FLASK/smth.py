089abb22e58aa8eb8ac1c3b4b6d1cedbbc2ca6de
Eraaalex <eralex135@gmail.com>
2022-10-04 21:04:17 +0300

    commit

--- FLASK/smth.py
+++ FLASK/smth.py
@@ -1,11 +1,13 @@
-
-import sys
-sys.path.append('../ORM')
-
-from models import *
-from service import *
-
-a=addFeedback(login='None4@none.com',text='Not Bad')
-# db.session.commit()
-fb=getFeedback()
-print(fb,len(fb), sep='\n')
+# # from flask import Flask, render_template, escape, abort, request
+# import sys
+# sys.path.append('../ORM')
+#
+# from models import *
+# from service import *
+#
+# app = Flask(__name__)
+#
+# a=addFeedback(login='None4@none.com',text='Not Bad')
+# # db.session.commit()
+# fb=getFeedback()
+# print(fb,len(fb), sep='\n')
